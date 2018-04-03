2944917b4cbc13deedbc2c206b21e1e830380713
Garrett Anderson <theboss@nulldestinations.com>
2018-04-03 14:39:27 -0700

    Experimental Input Fonts installation script

---
+++ install.d/20-input-font.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+OS="$(uname -s)"
+if ["$OS" = "Linux"]; then
+  sudo -u $dotfiles_usr mkdir $dotfiles_usr_home/.fonts
+  sudo -u $dotfiles_usr cp -rn $dotfiles_wd/input-font/Input_Fonts $dotfiles_usr_home/.fonts/
+fi
+
+if ["$OS" = "Darwin"]; then
+  sudo -u $dotfiles_usr cp -rn $dotfiles_wd/input-font/Input_Fonts $dotfiles_usr_home/Library/Fonts/
+fi
