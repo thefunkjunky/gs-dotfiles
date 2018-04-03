1b264011df520a84a901b150c03fb9f236d075fe
Garrett Anderson <theboss@nulldestinations.com>
2018-04-03 15:15:23 -0700

    Fixed Shiftit script

--- install.d/30-yos-shiftit.sh
+++ install.d/30-yos-shiftit.sh
@@ -4,4 +4,5 @@
 
 open ~/Applications/ShiftIt.app
 # This enables shiftit in security preferences, no seriously it does.
-sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" 'update access set allowed="1" where client like "%shiftit%";'
+sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" 'update access set allowed="1" where client like "%shiftit%";';
+killall Dock;
