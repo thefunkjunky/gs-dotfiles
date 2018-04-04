a7904cadf9060c202c72c63608634ac8c09872a0
Garrett Anderson <theboss@nulldestinations.com>
2018-04-04 14:59:28 -0700

    Made pcregrep colorful

--- home-config/bashrc.d/02-aliases.bash
+++ home-config/bashrc.d/02-aliases.bash
@@ -9,6 +9,7 @@ fi
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+alias pcregrep='pcregrep --color=auto'
 
 # Misc environment variables
 export EDITOR=vim
