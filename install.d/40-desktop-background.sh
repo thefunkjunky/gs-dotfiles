3a3f628cee127e5df7d69c39100d225d153b63af
Garrett Anderson <theboss@nulldestinations.com>
2018-04-06 10:37:11 -0700

    Fixed bash if statements bugs

--- install.d/40-desktop-background.sh
+++ install.d/40-desktop-background.sh
@@ -3,7 +3,7 @@
 OS="$(uname -s)"
 IMAGE_LOC="$dotfiles_wd/home-config/desktop-background.jpg"
 
-if ["$OS" = "Darwin"]; then
+if [ "$OS" = "Darwin" ]; then
   sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$IMAGE_LOC'";
   killall Dock;
 fi
--- install.d/40-sublime-text-config.sh
+++ install.d/40-sublime-text-config.sh
@@ -5,10 +5,10 @@ DOTF_ST3_USER_LOC="$dotfiles_wd/home-config/sublime-text/User"
 LINUX_SB3_PKG_LOC="$dotfiles_usr_home/.config/sublime-text-3/Packages/"
 OSX_SB3_PKG_LOC="$dotfiles_usr_home/Library/Application\ Support/Sublime\ Text\ 3/Packages/"
 
-if ["$OS" = "Linux"]; then
-  sudo -u $dotfiles_usr cp -rn $DOTF_ST3_USER_LOC $LINUX_SB3_PKG_LOC
+if [ "$OS" = "Linux" ]; then
+  sudo -u $dotfiles_usr cp -rf $DOTF_ST3_USER_LOC $LINUX_SB3_PKG_LOC
 fi
 
-if ["$OS" = "Darwin"]; then
-  sudo -u $dotfiles_usr cp -rn $DOTF_ST3_USER_LOC $OSX_SB3_PKG_LOC
+if [ "$OS" = "Darwin" ]; then
+  sudo -u $dotfiles_usr cp -rf $DOTF_ST3_USER_LOC $OSX_SB3_PKG_LOC
 fi
