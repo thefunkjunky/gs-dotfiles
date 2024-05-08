97ab0438e21eca02f2f41a2eef077d65079cf185
Garrett Anderson <g@devnull.rip>
2024-05-07 20:35:20 -0700

    Add bash completions

---
+++ home-config/bashrc.d/11-completions.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Bash completion
+eval "$(register-python-argcomplete pipx)"
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/ganderson/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/ganderson/google-cloud-sdk/completion.bash.inc'; fi
+
